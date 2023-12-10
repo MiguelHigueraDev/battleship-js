aa4d5b0b844ed07910c96626b83a92aa1518c5f4
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2023-12-10 18:08:25 -0300

    Add CSS reset and border-box

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -6,6 +6,7 @@ class Player {
   constructor () {
     this.board = new Gameboard()
     this.ships = this.createFleet()
+    this.placeDefaultShips()
   }
 
   createFleet () {
@@ -21,6 +22,14 @@ class Player {
     // Check if cell was hit before
     if (enemyBoard.checkIfCellIsHit(x, y)) return
     enemyBoard.receiveAttack(x, y)
+  }
+
+  placeDefaultShips () {
+    this.board.placeShip(this.ships[0], [[1, 5], [2, 5], [3, 5], [4, 5], [5, 5]])
+    this.board.placeShip(this.ships[1], [[1, 8], [2, 8], [3, 8], [4, 8]])
+    this.board.placeShip(this.ships[2], [[7, 5], [7, 6], [7, 7]])
+    this.board.placeShip(this.ships[3], [[1, 1], [1, 2]])
+    this.board.placeShip(this.ships[4], [[5, 2], [6, 2]])
   }
 }
 
